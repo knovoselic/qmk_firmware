e64670336890d445dae3a53c0a95d469ecea2fc6
Kristijan Novoselić <kristijan@codecons.com>
2021-12-30 20:01:24 +0100

    Get rid of the DIP switch code

--- keyboards/preonic/keymaps/knovoselic/keymap.c
+++ keyboards/preonic/keymaps/knovoselic/keymap.c
@@ -205,26 +205,6 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     return true;
 }
 
-bool dip_switch_update_user(uint8_t index, bool active) {
-    switch (index) {
-        case 0:
-            if (active) {
-                layer_on(_ADJUST);
-            } else {
-                layer_off(_ADJUST);
-            }
-            break;
-        case 1:
-            if (active) {
-                muse_mode = true;
-            } else {
-                muse_mode = false;
-            }
-    }
-    return true;
-}
-
-
 void matrix_scan_user(void) {
 #ifdef AUDIO_ENABLE
     if (muse_mode) {
